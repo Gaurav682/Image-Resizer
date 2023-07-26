8b6dd5fa9f77086a81285d7435021b84fdf13b04
Gaurav <123662776+Gaurav682@users.noreply.github.com>
2023-07-27 00:21:03 +0530

    main.py

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import cv2
 
-src = cv2.imread("johnny.jpg")
+src = cv2.imread("johnny.jpg")  # Specify Name/Address of image
 
-# cv2.imshow("Mr. Depp", src)
+#use to show your image with a title(in quotes)
+# cv2.imshow('Mr. Depp', src) 
 
 # percentage by which image is resized
 scale_percent = 50
 
 # calculate the 50 percent of original dimensions
-width = int(src.shape[1] * scale_percent / 100)  # numpy
+width = int(src.shape[1] * scale_percent / 100) 
 height = int(src.shape[0] * scale_percent / 100)
 
 # dsize
